9b96b69401459c49ff38013b76f6fae6eeb39d25
NiBeCa <nbcald@gmail.com>
2019-06-19 15:51:24 -0500

    inital folder structure and page set up

---
+++ client/src/pages/components/layout/Footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer>
+        &copy; Gorilla Syndicate 
+        {( new Date()).getFullYear()}
+    </footer>
+);
+
+export default Footer;
