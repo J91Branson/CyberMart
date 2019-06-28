d95e6878ddad1d64a4623f00e93cd953e2f0e065
J91Branson <jbcod4@gmail.com>
2019-06-27 22:58:35 -0500

    Contact page

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -1,19 +1,64 @@
 import React from 'react';
 import Content from "../../layouts/Content/Content";
+import {Container,Row,Col } from 'react-bootstrap';
+
+const divStyle = {
+  color: 'white',
+  backgroundImage: 'url("adventure-animal-day-1612847.jpg")',
+  height: '600px'
+  
+  
+};
 
 const Contact = () => (
   <Content>
-    <p>
-      Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis molestie urna.
-      Aliquam semper ultrices varius. Aliquam faucibus sit amet magna a ultrices. Aenean
-      pellentesque placerat lacus imperdiet efficitur. In felis nisl, luctus non ante euismod,
-      tincidunt bibendum mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-      posuere, eget tristique dui dapibus. Maecenas fermentum elementum faucibus. Quisque nec
-      metus vestibulum, egestas massa eu, sollicitudin ipsum. Nulla facilisi. Sed ut erat ligula.
-      Nam tincidunt nunc in nibh dictum ullamcorper. Class aptent taciti sociosqu ad litora
-      torquent per conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at rhoncus.
-      Etiam vel condimentum magna, quis tempor nulla.
-</p>
+    <Container fluid>
+  
+  
+  <Row>
+    
+  <Col xs={{ span: 12}}>
+  <div  style={divStyle}>
+
+      
+<h1> Contact Pets-buy
+</h1>
+
+<h3>
+We communicate well with pets, but we also<br/>
+speak human, contact us or talk to our bot.
+</h3>
+
+
+
+
+
+</div>
+
+    </Col>
+  </Row>
+</Container>
+<Container fluid>
+  
+  
+  <Row>
+    <Col xs={12} md={4}>
+     <h1>Customer Service Team</h1>
+     <h5>Monday-Friday</h5>
+     <h5>8:00AM-4:00PM CST </h5>
+     <h5>800-775-5555</h5>
+    </Col>
+    <Col xs={12} md={4}>
+      <h1>Email</h1>
+      <h5>petsbuy@gmail.com</h5>
+    </Col>
+    <Col xs={12} md={4}>
+      <h1>Corporate Office</h1>
+      <h5>1970 Disco Drive</h5>
+      <h5>Grooveville Alaska</h5>
+    </Col>
+  </Row>
+</Container>   
   </Content>
 );
 
