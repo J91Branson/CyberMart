2e32d9992ea4a1a74e121318f242a1454afaf3ef
CaitlynS <theprideignites@gmail.com>
2019-07-06 16:37:53 -0500

    Styling - Team page

--- client/src/layouts/Footer/Footer.js
+++ client/src/layouts/Footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => (
             <hr className="footerH"/>
             <Link to="/contact" style={{color: "#484E5D"}}><i className="far fa-address-book"></i> Contact </Link>
             |
-            <Link to="/about" style={{color: "#484E5D"}}> <i className="fas fa-book-reader"></i> Team </Link>
+            <Link to="/about" style={{color: "#484E5D"}}> <i className="fas fa-users"></i> Team </Link>
         </div>
 
         &copy; {(new Date()).getFullYear()} Gorilla Syndicate
