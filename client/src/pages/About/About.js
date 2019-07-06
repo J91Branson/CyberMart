ab4425e1a7d546e588e6a24a0910bc13770241ce
J91Branson <jbcod4@gmail.com>
2019-07-06 14:58:54 -0500

    images added

--- client/src/pages/About/About.js
+++ client/src/pages/About/About.js
@@ -4,29 +4,37 @@ import React from 'react';
 // Import Files/Components
 import Content from "../../layouts/Content/Content";
 
+
+const ImageStyle={height:"200px"};
+
 const About = () => (
-  <Content>
+    <Content>
     <h3>
+      <img style={ImageStyle} src={process.env.PUBLIC_URL+"/assets/JB.png"}/>
       <h2>Joshua Branson</h2>
       <h5>Front-end and Design</h5>
       <h5><a href=""></a><i class="fab fa-github-square"></i><a href=""></a><i class="fab fa-linkedin"></i></h5>
     </h3>
     <h3>
+      <img style={ImageStyle} src={process.env.PUBLIC_URL+"/assets/Caitlyn.png"}/>
       <h2>Caitlyn Sifuentes</h2>
       <h5>Front-end and Design</h5>
       <h5><a href=""></a><i class="fab fa-github-square"></i><a href=""></a><i class="fab fa-linkedin"></i></h5>
     </h3>
     <h3>
+      <img style={ImageStyle} src={process.env.PUBLIC_URL+"/assets/G.Sean.png"}/>
       <h2>G. Sean Massingill</h2>
       <h5>Front-end and Design</h5>
       <h5><a href=""></a><i class="fab fa-github-square"></i><a href=""></a><i class="fab fa-linkedin"></i></h5>
     </h3>
     <h3>
+      <img style={ImageStyle} src={process.env.PUBLIC_URL+"/assets/Nina.png"}/>
       <h2>Nina Calderone</h2>
       <h5>Front-end and Design</h5>
       <h5><a href=""></a><i class="fab fa-github-square"></i><a href=""></a><i class="fab fa-linkedin"></i></h5>
     </h3>
     <h3>
+      <img style={ImageStyle} src={process.env.PUBLIC_URL+"/assets/Jackson.png"}/>
       <h2>Jackson Stein</h2>
       <h5>Front-end and Design</h5>
       <h5><a href=""></a><i class="fab fa-github-square"></i><a href=""></a><i class="fab fa-linkedin"></i></h5>
